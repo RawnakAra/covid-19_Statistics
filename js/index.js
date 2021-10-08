ac4eedb1c95418392a4c659216670859a43121bf
RawnakAra <rawnak96@gmail.com>
2021-10-08 23:25:48 +0300

    first

--- js/index.js
+++ js/index.js
@@ -2,6 +2,14 @@ let coronaURL = 'https://corona-api.com/countries'
 let countriesURl = 'https://cors-anywhere.herokuapp.com/https://restcountries.herokuapp.com/api/v1'
 //let countriesURl = 'https://restcountries.herokuapp.com/api/v1'
 let countriesResult, covidDataResult, chosenContinent, graphObject;
+const asia = document.querySelector('#asia'),
+    africa = document.querySelector('#africa'),
+    america = document.querySelector('#america'),
+    europe = document.querySelector('#europe'),
+    oceania = document.querySelector('#oceania'),
+    CountriesContainer = document.querySelector('.CountriesContainer')
+
+
 async function getCountries() {
     let countries = await ((await fetch(countriesURl)).json())
     countries = countries.map(element => {
@@ -30,17 +38,22 @@ async function getCOVIDData() {
     //return covidData;
     //let mahde = JSON.parse(sessionStorage.getItem('countries')) //USE THIS IN OTHER CODE
 }
+console.log(countriesResult);
 function getContinent(continentIWant, typeOfStatistic) {
-    let arrayOfCountriesNames = [], arrayOfCountriesCovidData = [];
+    let arrayOfCountriesCovidData = [], arrayOfCountriesNames = [];
     console.log(countriesResult);
     for (let i = 0; i < countriesResult.length; i++) {
         if (countriesResult[i].region == continentIWant) {
-            arrayOfCountriesNames.push(countriesResult[i].name);
-            arrayOfCountriesCovidData.push(covidDataResult[i][typeOfStatistic])
+            for (let j = 0; j < covidDataResult.length; j++)
+                if (covidDataResult[j].name == countriesResult[i].name && covidDataResult[j] != undefined) {
+                    arrayOfCountriesCovidData.push(covidDataResult[j][typeOfStatistic])
+                    arrayOfCountriesNames.push(countriesResult[i].name);
+                }
         }
     }
     return { arrayOfCountriesNames, arrayOfCountriesCovidData }
 }
+
 function buildChart(charttype, continent, statistictype) {
     let dataRes = getContinent(continent, statistictype)
     if (graphObject)
@@ -63,16 +76,57 @@ function buildChart(charttype, continent, statistictype) {
         }
     })
 }
+// async function pastCountries() {
+//     CountriesContainer.textContent = ''
+// }
 async function run() {
-    await getCountries()
-    await getCOVIDData()
+     await getCountries()
+     await getCOVIDData()
     countriesResult = JSON.parse(sessionStorage.getItem('countries'))
     covidDataResult = JSON.parse(sessionStorage.getItem('covidData'))
     console.log(countriesResult);
     console.log(covidDataResult);
 }
-document.querySelector('#asia').addEventListener('click', async function () {
+
+asia.addEventListener('click', async function () {
     await run()
     buildChart('line', 'Asia', 'confirmed')
     chosenContinent = 'Asia';
 })
+africa.addEventListener('click', async function () {
+    await run()
+    buildChart('line', 'Africa', 'confirmed')
+    chosenContinent = 'Africa';
+})
+america.addEventListener('click', async function () {
+    await run()
+    buildChart('line', 'America', 'confirmed')
+    chosenContinent = 'America';
+})
+europe.addEventListener('click', async function () {
+    await run()
+    buildChart('line', 'Europe', 'confirmed')
+    chosenContinent = 'Europe';
+})
+oceania.addEventListener('click', async function () {
+    await run()
+    buildChart('line', 'Oceania', 'confirmed')
+    chosenContinent = 'Oceania';
+})
+
+// document.querySelector('.condition').addEventListener('click',async(chosenContinent)=>{
+//     await run()
+//     buildChart('line', chosenContinent, 'condition')
+// })
+// document.querySelector('.death').addEventListener('click',async(chosenContinent)=>{
+//     await run()
+//     buildChart('line', chosenContinent, 'deaths')
+// })
+// document.querySelector('.recovered').addEventListener('click',async(chosenContinent)=>{
+//     await run()
+//     buildChart('line', chosenContinent, 'recovered')
+// })
+// document.querySelector('.confirmed').addEventListener('click',async(chosenContinent)=>{
+//     await run()
+//     buildChart('line', chosenContinent, 'confirmed')
+// })
